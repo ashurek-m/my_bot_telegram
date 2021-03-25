7f61c6852caf5b45d283db3e4481016aa266a8de
oshurek_m <max.ashurek@gmil.com>
2021-03-25 18:10:15 +0300

    бот уровень 1

--- venv/agent_smith_bot.py
+++ venv/agent_smith_bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(constant.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Я готов выполнять задание')
 
 
 
