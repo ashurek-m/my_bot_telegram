af6c07bf3388356dd7e516139113f04ec58dfeee
oshurek_m <max.ashurek@gmil.com>
2021-03-26 14:11:50 +0300

    бот уровень 1 - поиск по итему

--- venv/agent_smith_bot.py
+++ venv/agent_smith_bot.py
@@ -4,13 +4,15 @@ import exped
 import pandas as pd
 
 bot = telebot.TeleBot(constant.token)
-df = exped.df_exped
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Я готов выполнять задание')
 
 @bot.message_handler(content_types=['text'])
 def lock_item(message):
-    bot.send_message(message.chat.id, 'df.info()')
+    df = exped.df_exped
+    item = int(message.text)
+    bot.send_message(message.chat.id, exped.item_filter(item, df))
 
 bot.polling(none_stop=True, interval=0)
