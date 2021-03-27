87cf6f147fd3a5ec0182e154ecf20a2b3a6fb1b9
oshurek_m <max.ashurek@gmil.com>
2021-03-27 15:59:50 +0300

    бот уровень 1 - поиск по заказу

--- venv/agent_smith_bot.py
+++ venv/agent_smith_bot.py
@@ -11,21 +11,36 @@ def start_message(message):
     bot.send_message(message.chat.id, support.instruction)
 
 @bot.message_handler(commands=['item'])
-def start_message(message):
+def item_message(message):
     bot.send_message(message.chat.id, 'введите итем')
-    bot.register_next_step_handler(message, lock_item)
+    bot.register_next_step_handler(message, 'item' + lock_item)
 
+@bot.message_handler(commands=['order'])
+def order_message(message):
+    bot.send_message(message.chat.id, 'введите номер интересующего вас заказа')
+    bot.register_next_step_handler(message, 'order' + lock_item)
 
 @bot.message_handler(content_types=['text'])
 def lock_item(message):
-    try:
-        df = exped.df_exped
-        item = int(message.text)
-        bot.send_message(message.chat.id, exped.item_filter(item, df))
-    except KeyError:
-        bot.send_message(message.chat.id, 'такого итема нет в базе')
-    except ValueError:
-        bot.send_message(message.chat.id, 'введите номер итема цифрами')
+    if 'item' in message.text:
+        try:
+            df = exped.df_exped
+            item = int(message.text)
+            bot.send_message(message.chat.id, exped.item_filter(item, df))
+        except KeyError:
+            bot.send_message(message.chat.id, 'такого итема нет в базе')
+        except ValueError:
+            bot.send_message(message.chat.id, 'введите номер итема цыфрами')
+    elif 'order' in message.text:
+        try:
+            df = exped.df_exped
+            order = int(message.text)
+            bot.send_message(message.chat.id, exped.text_filter_by_order(order, df))
+        except KeyError:
+            bot.send_message(message.chat.id, 'такого заказа нет в базе')
+            bot.register_next_step_handler(message, lock_order)
+        except ValueError:
+            bot.send_message(message.chat.id, 'введите номер заказа цыфрами')
 
 
 bot.polling(none_stop=True, interval=0)
--- venv/exped.py
+++ venv/exped.py
@@ -14,12 +14,34 @@ def item_filter(item, data):
     return tech_proc
 
 
-def filter_by_orders():
+def filter_by_orders(order, data):
+    filter_list = []
+    filter_list.append(order)
+    filter_list.append(int('8' + str(order)))
+    df_exped_filtr = data[data['Индекс RTB'].isin(filter_list)].reset_index(drop=True)
+    return df_exped_filtr
 
-    return
 
-def text_1(t):
-    return t
+def text_filter_by_order(order, data):
+    df = filter_by_orders(order, data)
+    if df.shape[0] !=0:
+        list_2 = []
+        list_3 = []
+        for i in range(df.shape[0]):
+            list_1 = []
+            list_1.append(str(int(df.loc[i, 'Индекс RTB'])))
+            list_1.append(str(df.loc[i, 'Наименование детали']))
+            list_1.append(str(df.loc[i, 'Отметка о выполнении']))
+            list_1.append(str(df.loc[i, 'Потенциальные проблемы']))
+            list_1.append(str(int(df.loc[i, 'N° ITEM'])))
+            list_2.append(list_1)
+        for j in range(len(list_2)):
+            text_proc = ', '.join(list_2[j])
+            list_3.append(text_proc)
+        text_proc_1 = ';\n\n'.join(list_3)
+    else:
+        text_proc_1 = 'такого заказа нет в базе'
+    return text_proc_1
 
 
 path = 'W:\\Theoretical Planning\\03 - План отгрузки (Planification)\\План отгрузки\\EXPED. IDM-Direct.xlsb'
