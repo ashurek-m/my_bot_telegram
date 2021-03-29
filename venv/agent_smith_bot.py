2872410636adc1755dd2ff05973014d10bec47bf
oshurek_m <max.ashurek@gmil.com>
2021-03-29 20:32:54 +0300

    бот уровень 1 - создание кнопок

--- venv/agent_smith_bot.py
+++ venv/agent_smith_bot.py
@@ -8,7 +8,6 @@ import support
 bot = telebot.TeleBot(constant.token)
 
 @bot.message_handler(commands=['start'])
-
 def start_message(message):
     bot.send_message(message.chat.id, support.instruction)
 
