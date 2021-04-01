19a1519ea3171f2e747fde2f9a0f7c62797093e9
ashurek-m <max.ashurek@gmail.com>
2021-04-01 21:39:11 +0300

    бот на аиограме 1

--- venv/agent_smith_bot.py
+++ venv/agent_smith_bot.py
@@ -7,5 +7,5 @@ bot = aiogram.Bot(constant.token)
 dp = aiogram.Dispatcher(bot, loop)
 
 if __name__ == '__agent_smith_bot__':
-    from handlers import dp
-    aiogram.executor.start_polling(dp)
+    from handlers import dp, send_admin_ms
+    aiogram.executor.start_polling(dp, on_startup=send_admin_ms)
--- venv/handlers.py
+++ venv/handlers.py
@@ -1,4 +1,6 @@
 from agent_smith_bot import bot, dp
-import aiogram
+from aiogram.types import Message
 import constant
 
+async def send_admin_ms():
+    await bot.send_message(chat_id=massage.from_user.id, text='Бот запущен')
